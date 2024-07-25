28f7d6237f419d9b466ccc0ae5ed2ddc3a2bf9ae
Pareesa Kamgar Dayhoff <pareesakd1118@gmail.com>
2024-07-25 14:50:01 -0700

    Update server

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,5 @@
 function getAllScientists() {
-    return fetch("https://gentle-sierra-88471-456c461e0158.herokuapp.com/api/v1/womenscientists")
+    return fetch("https://shes-a-scientist-0e61c1f67e15.herokuapp.com/api/v1/womenscientists")
     .then(res => {
         if(!res.ok){
         throw new Error(`${res.status} Error: ${res.statusText}. Unable to retrieve scientists at this time. Please try again later.`)
@@ -10,7 +10,7 @@ function getAllScientists() {
 }
 
 function getSingleScientist(id) {
-    return fetch(`https://gentle-sierra-88471-456c461e0158.herokuapp.com/api/v1/womenscientists/${id}`)
+    return fetch(`https://shes-a-scientist-0e61c1f67e15.herokuapp.com/api/v1/womenscientists/${id}`)
     .then(res => {
         if(!res.ok){
             throw new Error(`${res.status} Error: ${res.statusText}. Unable to retrieve scientist at this time. Please try again later.`)
